010a1a58bfe084e9e17809107d342a8bf33205cc
GWeronika <126601389+GWeronika@users.noreply.github.com>
2023-05-24 17:37:08 +0200

    Delete Administrator.java

--- Administrator.java
+++
@@ -1,8 +0,0 @@
-/**
- * tworzy administratora systemu banku internetowego
- */
-public class Administrator extends Pracownik {
-    public Administrator (String imie, String nazwisko, String PESEL, String login, String haslo) {
-        super(imie, nazwisko, PESEL, login, haslo);
-    }
-}
